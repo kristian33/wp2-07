44b2bae46e3da4808a2b68cd441462a18b2160b3
kristianklimek <klimekkristian@gmail.com>
2019-01-24 10:29:05 +0100

    Create index.php
    LOL

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="cs" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>kok</title>
+  </head>
+  <body>
+<table>
+  <?php
+  for ($i=0; $i < 10 ; $i++) {
+?><tr><?php
+    for ($j=0; $j < 10 ; $j++) {
+      ?><td><?php
+      echo rand(0,100);
+      ?></td><?php
+    }
+?></tr><?php
+  }
+    ?>
+  </table>
+  </body>
+  </html>
